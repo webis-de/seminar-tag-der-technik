3659fb3ddc552b155a138ac97ae66f857b585615
Tim Hagen <timhagenmail@googlemail.com>
2024-10-09 13:49:04 +0200

    current state

--- docker-content/tag-der-technik/experiments/definitions.py
+++ docker-content/tag-der-technik/experiments/definitions.py
@@ -5,16 +5,6 @@ from openai import OpenAI
 
 client = OpenAI(api_key="blah", base_url="https://llm.srv.webis.de/v1")
 
-"""
-def fetch_response(input: str):
-    completion = client.chat.completions.create(
-        model="default",
-        messages=[{"role": "user", "content": input}],
-        # temperature=0,
-    )
-    return completion.choices[0].message.content
-"""
-
 
 def fetch_completion(systemprompt: Optional[str], userprompt: str, examples: list[tuple[str, str]] = []):
     messages = [{"role": "system", "content": systemprompt}] if systemprompt is not None else []
@@ -34,8 +24,7 @@ def fetch_response(inp: str) -> str:
     try:
         resp = extract_between(resp, "<Response>", "</Response>")
     except:
-        print(resp)
-        raise
+        pass
     return resp
 
 
