b6173855f8d832057159517b5e2165a54c7773d5
Tim Hagen <timhagenmail@googlemail.com>
2024-10-09 08:23:36 +0200

    current state

--- docker-content/tag-der-technik/experiments/definitions.py
+++ docker-content/tag-der-technik/experiments/definitions.py
@@ -1,19 +1,43 @@
 import re
+from typing import Optional
 
 from openai import OpenAI
 
 client = OpenAI(api_key="blah", base_url="https://llm.srv.webis.de/v1")
 
-def fetch_completion(systemprompt: str, userprompt: str, examples: list[tuple[str, str]] = []):
-    messages = [{"role": "system", "content": systemprompt}]
+"""
+def fetch_response(input: str):
+    completion = client.chat.completions.create(
+        model="default",
+        messages=[{"role": "user", "content": input}],
+        # temperature=0,
+    )
+    return completion.choices[0].message.content
+"""
+
+
+def fetch_completion(systemprompt: Optional[str], userprompt: str, examples: list[tuple[str, str]] = []):
+    messages = [{"role": "system", "content": systemprompt}] if systemprompt is not None else []
     for input, output in examples:
         messages.extend([{"role": "user", "content": input}, {"role": "assistant", "content": output}])
     messages.append({"role": "user", "content": userprompt})
     completion = client.chat.completions.create(
         model="default",
         messages=messages,
+        # temperature=0,
     )
     return completion.choices[0].message.content
 
+
+def fetch_response(inp: str) -> str:
+    resp = fetch_completion(None, inp)
+    try:
+        resp = extract_between(resp, "<Response>", "</Response>")
+    except:
+        print(resp)
+        raise
+    return resp
+
+
 def extract_between(input: str, left_delim: str, right_delim: str):
-    return re.findall(rf'{left_delim}(.*?){right_delim}', input)[0]
+    return re.findall(rf'{left_delim}(.*?){right_delim}', input, re.MULTILINE | re.DOTALL)[0]
